ac1e974155be1df1bf53a2c9bece0e91c67e3240
Edward Boland <mr.eboland@gmail.com>
2020-05-20 17:28:58 -0400

    extensive media query work done

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,21 @@
 // Main
 
 // code courtesy of https://www.w3schools.com/howto/howto_js_read_more.asp
-function myFunction() {
-    const dots = document.getElementById("dots");
-    const moreText = document.getElementById("more");
-    const btnText = document.getElementById("myBtn");
+// function myFunction() {
+//     const dots = document.getElementById("dots");
+//     const moreText = document.getElementById("more");
+//     const btnText = document.getElementById("myBtn");
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
-    }
-}
-
-const toggleButton = document.querySelector('.toggle-menu');
-const navBar = document.querySelector('.nav-bar');
-toggleButton.addEventListener('click', function () {
-    navBar.classList.toggle('toggle');
-});
-
-
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//     if (dots.style.display === "none") {
+//         dots.style.display = "inline";
+//         btnText.innerHTML = "Read more";
+//         moreText.style.display = "none";
+//     } else {
+//         dots.style.display = "none";
+//         btnText.innerHTML = "Read less";
+//         moreText.style.display = "inline";
+//     }
+// }
 
 // Resume
 
